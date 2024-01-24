b827960928e6ff50643affc1d4741dd2f6c22dd9
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2024-01-23 23:33:32 -0700

    went will emailjs for emails

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,13 @@
-import axios from 'axios';
+
 import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 
 export default function Contact() {
-    const [data, setData] = useState({
-        name: '',
-        email: '',
+
+    const [form, setForm] = useState({
+        user_name: '',
+        user_email: '',
         message: '',
         sent: false,
         buttonText: 'Submit',
@@ -14,16 +17,16 @@ export default function Contact() {
     const handleChange = (event) => {
         const { name, value } = event.target;
 
-        setData({
-            ...data,
+        setForm({
+            ...form,
             [name]: value,
         });
     };
 
     function clearForm() {
-        setData({
-            name: '',
-            email: '',
+        setForm({
+            user_name: '',
+            user_email: '',
             message: '',
             sent: false,
             buttonText: 'Submit',
@@ -33,44 +36,30 @@ export default function Contact() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(data),
-            setData({
-                ...data,
 
-                buttonText: 'Sending...'
-            })
+        setForm({
+            ...form,
 
-        axios.post('api/sendmail', data)
-            .then(res => {
-                if (res.data.result !== 'success') {
-                    setData({
-                        ...data,
-                        buttonText: 'Tried but failed',
-                        sent: false,
-                        err: 'fail'
-                    })
-                    setTimeout(() => {
-                        clearForm()
-                    }, 6000)
-                } else {
-                    setData({
-                        ...data,
-                        sent: true,
-                        buttonText: 'Sent',
-                        err: 'success'
-                    })
-                    setTimeout(() => {
-                        clearForm();
-                    }, 6000)
-                }
-            }).catch((err) => {
-                console.log('Reason: ' + err)
-                setData({
-                    ...data,
-                    buttonText: 'Failed to send',
+            buttonText: 'Sending...'
+        })
+
+        emailjs.sendForm('service_l5rrzib', 'template_jydqrkh', form.current, 'tTIbuRoI0TfwysV2H')
+            .then((result) => {
+                console.log(result.text);
+                clearForm();
+            }, (error) => {
+                console.log(error.text);
+                setForm({
+                    ...form,
+                    buttonText: 'Failed',
+                    sent: false,
                     err: 'fail'
                 })
-            })
+                setTimeout(() => {
+                    clearForm();
+                }, 6000)
+            });
+
     };
 
 
@@ -81,38 +70,38 @@ export default function Contact() {
                 <center className="font-light mb-8">Please leave me your information. Along with any comments.</center>
             </h2>
             <div className="flex justify-center items-center">
-                <form className="w-1/2 text-left">
-                    <label className="block mb-4">
-                        Name: <input
-                            onChange={handleChange}
-                            value={data.name}
-                            name="name"
-                            type="text"
-                            className="rounded px-2 w-full" />
-                    </label>
+                <form ref={form} className="w-1/2 text-left">
+                    <label className="block">Name: </label>
+                    <input
+                        onChange={handleChange}
+                        value={form.user_name}
+                        name="user_name"
+                        type="text"
+                        className="rounded px-2 w-full mb-4" />
 
-                    <label className="block mb-4">
-                        Email: <input
-                            onChange={handleChange}
-                            value={data.email}
-                            name="email"
-                            type="email"
-                            className="rounded px-2 w-full" />
-                    </label>
 
-                    <label className="block mb-4">
-                        Message: <textarea
-                            onChange={handleChange}
-                            value={data.message}
-                            name="message"
-                            rows={4}
-                            className="rounded px-2 w-full" />
-                    </label>
+                    <label className="block">Email: </label>
+                    <input
+                        onChange={handleChange}
+                        value={form.user_email}
+                        name="user_email"
+                        type="email"
+                        className="rounded px-2 w-full mb-4" />
+
+
+                    <label className="block">Message: </label>
+                    <textarea
+                        onChange={handleChange}
+                        value={form.message}
+                        name="message"
+                        rows={4}
+                        className="rounded px-2 w-full mb-4" />
+
 
                     <button
                         onClick={handleSubmit}
                         className="bg-blue-600 p-2 text-white hover:text-blue-600 hover:bg-sky-400 ml-4 rounded">
-                        {data.buttonText}
+                        Submit
                     </button>
                 </form>
             </div>
